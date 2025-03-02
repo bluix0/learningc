a293eb25af5b75848b99bae0398a573c27487b22
Bluix <net606@proton.me>
2025-03-02 18:36:59 +0800

    Add files via upload

---
+++ c13_数组习题2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<ctype.h>
+/*初版
+int main()
+{   
+    for(char n;(n= getchar())!='\n';)
+        if(isupper(n)) {
+            n='A'+'Z'-n;
+            printf("%c",n);
+        }
+        else if(islower(n)) {
+            n='a'+'z'-n;
+            printf("%c",n);
+        }
+        else
+            printf("%c",n);
+
+    return 0;
+}
+*/
+int main()
+{
+char arr[100]={0};
+char arr2[100]={0};
+fgets(arr,sizeof(arr),stdin);
+for(int i=0;arr[i]!=0;i++)
+        if(isupper(arr[i])) {
+            arr2[i]='A'+'Z'-arr[i];
+        }
+        else if(islower(arr[i])) {
+            arr2[i]='a'+'z'-arr[i];
+        }
+        else
+            arr2[i]=arr[i];
+printf("密码：%s",arr);
+printf("原文：%s",arr2);
+return 0;
+}
